6e76166fb4a6dff6dda92321dc190a692f5536c1
Roderik Adang <radang@mangrove.nl>
2020-10-21 10:53:22 +0200

    add club mock

--- src/lambda/club.js
+++ src/lambda/club.js
@@ -1,13 +1,13 @@
 exports.handler = async (event, context) => {
   return {
     statusCode: 200,
-    body: {
+    body: JSON.stringify({
       "id": 1,
       "logo": "https://myalbum.com/photo/7Rp97Nn9syMC/360.jpg",
       "name": "Ajax",
       "type": "Voetbalclub",
       "escription": "Amsterdamsche Football Club Ajax, kortweg AFC Ajax of gewoon Ajax, is een Nederlandse profvoetbalclub uit Amsterdam. De club is opgericht op 18 maart 1900 en is een van de drie traditionele topclubs in Nederland. Sinds de oprichting van de Eredivisie speelt Ajax onafgebroken in deze hoogste divisie. Ajax is 34 keer kampioen van Nederland geworden.\n\nOp de UEFA-ranglijst van de beste clubs in Europa staat AFC Ajax in het seizoen 2019/20 op 27 mei 2020 als beste Nederlandse club op de 21e plaats met 69.500 punten.[5] Het Duitse Kicker Sportmagazin noemde Ajax de op een na beste voetbalclub van de twintigste eeuw. Volgens de IFFHS is Ajax de op zes na succesvolste Europese club van de twintigste eeuw en werd het in 1992 door de IFFHS verkozen als Wereldclubteam van het Jaar.\n\nIn 1995 werd Ajax door het Engelse voetbaltijdschrift World Soccer uitgeroepen tot Wereldteam van het Jaar. In 2000 eindigde Ajax tijdens de FIFA World gala-verkiezing, samen met het Braziliaanse Santos FC, op een gedeelde vijfde plaats van beste voetbalclubs van de twintigste eeuw. Daarnaast werd Ajax viermaal uitgeroepen tot Europees team van het Jaar in 1969, 1971, 1972, en 1973 en vijfmaal uitgeroepen tot Nederlands Sportploeg van het Jaar in 1968, 1969, 1972, 1987 en 1995.[6] Het is een van de zes Europese clubs die het recht heeft de Badge of Honour op de mouw te dragen door drie keer winst op rij van de Europacup I, in 1971, 1972 en 1973. Die drie keer winst op rij is ook reden om als een van vijf clubs de beker te behouden. De club heeft sinds 1998 een beursnotering, AFC Ajax NV.",
       "liters": 45.0
-    }
+    })
   };
 };
